ad406cb0483b8f313c3ddf881508b0ba3ae9b301
Arlemi <arlemi.turpault@gmail.com>
2017-04-14 17:51:17 +0200

    add url in the parameters

--- config.js
+++ config.js
@@ -1,15 +1,18 @@
 var config = {
 	watson: {
 		language_translation: {
+			url:'',
 			username: '',
 			password: '',
 			version: 'v2'
 		},
 		visual_recognition: {
+			url:'',
 			api_key: '',
 			version_date: '2016-05-20'
 		},
 		text_to_speech: {
+			url:'',
 			username: '',
 			password: '',
 			version: 'v1'
